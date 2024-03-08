dce6fb76e85de1273146b26ad571810da585a8c6
SafaaHd <146643742+SafaaHd@users.noreply.github.com>
2024-03-08 18:34:36 +0300

    added api data and dynamic rount

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
+skills= [
+{
+    'id':1,
+    'title':'Data Aanalyst',
+    'skill' :'Data cleasing,Hadoop'
+},
+{
+    'id':2,
+    'title':'Statistical Machine learning',
+    'skill' :'Spark,Python,Hadoop'
+},
 
+{
+    'id':3,
+    'title':'General Development',
+    'skill' :'Web development Backend using Python,php'
+}
+]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',skills=skills,prof_name='Safaa')
+
+@app.route("/api/skills")
+def list_skills():
+    return jsonify(skills)
